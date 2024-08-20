7551c67290f985eb8aac0dc5e71a1874293ca88d
suruthisuji <suruthin78149@gmail.com>
2024-08-20 16:48:30 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -28,5 +28,4 @@ def result():
     return render_template('result.html', entry=entry)
 
 if __name__ == '__main__':
-    app.run(debug=True)
- app.run(debug=True, port=8080)
+     app.run(debug=True, port=8080)
